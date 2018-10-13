8b0253549f91c9a3a01b44e5fc0df99fcec3b4de
ToluFemiTayo <omodarafemi2123@gmail.com>
2018-10-13 20:30:05 +0100

    project__one in progress

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,32 +1,9 @@
-// var _body = document.getElementsByTagName("body")[0];
-// // console.log({_body});
-// // _body.style.backgroundColor = "#222222";
-// // _body.className = "black";
-// var _name = document.getElementById("formGroupEnteryourfullname");
-// var nameHelp = document.getElementById("nameHelp");
-// // console.log({_name});
-// _name.addEventListener("keyup", function(e){
-//     // console.log(e.target.value);
-//     var value = e.target.value;
-//     if (value.length < 4 ) {
-//         console.log(value + " must be longer than two ");
-//         this.classList.add("is-invalid");
-//         nameHelp.classList.remove("d-none");
-//     }
-//     else {
-//         this.classList.remove("is-invalid");
-//         nameHelp.classList.add("d-none");
-//     }
-// });
+// Js on form
 var _form = document.getElementsByClassName("form__fill")[0];
 var _inputs = Array.from(_form.getElementsByClassName("form-control"));
 var _error = 0;
-// array.from convert html collection to arrays properties
-// console.log(_inputs);
 var keyupHandler = function (e) {
-  // console.log(e.target.nextElementSibling);
   var _next = e.target.nextElementSibling;
-  // console.log(e.target.name);
   if (e.target.name === "name" ) {
    if (e.target.value.length < 4) {
     this.classList.add("is-invalid");
@@ -37,12 +14,9 @@ var keyupHandler = function (e) {
     _next.classList.add("d-none");
    }
   }
-  
   else if (e.target.name === "email") {
-   // console.log("email");
    var regExp = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
    var value = e.target.value;
-   // console.log(regExp.test(String(value).toLowerCase()));
    if (!regExp.test(String(value).toLowerCase())) {
     this.classList.add("is-invalid");
     _next.classList.remove("d-none");
@@ -53,7 +27,6 @@ var keyupHandler = function (e) {
    }
   }
   else if (e.target.name === "message") {
-   // console.log("message");
    if (e.target.value.length < 10) {
     this.classList.add("is-invalid");
     _next.classList.remove("d-none");
@@ -70,4 +43,5 @@ _inputs.forEach(function (eachInputs) {
    });
  _form.addEventListener("submit")
 e.preventDefault();
+// Js on form
 
